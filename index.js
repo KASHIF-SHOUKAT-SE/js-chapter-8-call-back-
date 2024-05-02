d99bfed531286219fade4794f0a80d1cb26ba349
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-05-02 14:19:20 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,119 @@
+// syncronous 
+// let a = prompt("what is your name");
+// let b = prompt("what is your age");
+// let c = prompt("what is your favirote color");
+// console.log(a + "is"+b+"year old and has" + c + "favorite colo.");
+
+
+// asyncronous 
+// console.log("start");
+// setTimeout(function(){
+//     console.log("hey i am ther")
+// },3000);
+// console.log("end")
+
+// callbacks
+// function loadScript (src){
+//     var sript = document.createElement("script");
+//     sript.src = src;
+//     sript.onload = function(){
+//         console.log("loaded script with src:"+ src)
+//     }
+//     document.body.appendChild(sript);
+// }
+// loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
